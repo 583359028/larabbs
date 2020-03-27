77213750c463b84e686c9736a16aae0b9546c6cd
xiao <dream@xiaodeMacBook-Pro.local>
2020-03-28 01:41:47 +0800

    用户话题列表

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -31,7 +31,7 @@
                          </a>
                          <span> • </span>
                          <i class="far fa-clock"></i>
-                         <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">{{ $topic->updated_at->diffForHumans() }}</span>
+                         <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">{{ if_query('order', 'recent') ? $topic->created_at->diffForHumans() . '发布' : $topic->updated_at->diffForHumans() . '回复' }}</span>
                      </small>
 
 
