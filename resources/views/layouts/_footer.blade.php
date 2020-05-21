a97502071ac54df5e6bc076a7bf7b8d0046211a9
xiao <dream@xiaodeMacBook-Pro.local>
2020-05-21 20:50:11 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
       由 <a href="https://weibo.com/u/6243577870?is_hot=1" target="_blank">Relife飞</a> 设计和编码 <span style="collor: #e275275;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    <p class="float-right"><a href="{{ setting('contact_email', 'mailto:name@email.com') }}">联系我们</a></p>
   </div>
 </footer>
